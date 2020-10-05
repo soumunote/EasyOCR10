154c5df32d4515b2bee190fdaf1661104ee32594
Note Soumu <soumunote@gmail.com>
2020-10-05 20:45:09 +0900

    Scan Success!!!

--- EasyOCR10/ScanningDialog.xaml.cs
+++ EasyOCR10/ScanningDialog.xaml.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Media.Ocr;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -24,13 +28,41 @@ namespace EasyOCR10
             this.InitializeComponent();
         }
 
+        private async void ContentDialog_Opened(ContentDialog sender, ContentDialogOpenedEventArgs args)
+        {
+            await doScanAsync();
+        }
+
         private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
         }
 
-        void doScan()
+        async Task doScanAsync()
         {
+            var ocrEngine = OcrEngine.TryCreateFromUserProfileLanguages();
+            if (ocrEngine == null)
+            {
+                //TODO
+                //rootPage.NotifyUser();
+            }
+            // https://github.com/microsoft/Windows-universal-samples/blob/master/Samples/OCR/cs/OcrCapturedImage.xaml.cs
+            // https://docs.microsoft.com/ja-jp/windows/uwp/files/quickstart-reading-and-writing-files
+            // https://docs.microsoft.com/ja-jp/windows/uwp/audio-video-camera/imaging
+            // https://codezine.jp/article/detail/11072?p=2
+            // https://www.tnksoft.com/blog/?p=5634
+            var storageFolder = await Windows.Storage.StorageFolder.GetFolderFromPathAsync(@"c:\tmp\ticket");
+            var imageFile = await storageFolder.GetFileAsync("1.jpg");
+            using (var stream = await imageFile.OpenAsync(FileAccessMode.Read))
+            {
+                var decoder = await BitmapDecoder.CreateAsync(stream);
+                var bitmap = await decoder.GetSoftwareBitmapAsync();
+                var ocrResult = await ocrEngine.RecognizeAsync(bitmap);
+                var text = ocrResult.Text;
+
+            }
 
         }
+
+
     }
 }
