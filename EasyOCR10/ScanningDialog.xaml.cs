6e840e5b6fa06fbed97f51ef7bc37fbd96627ab5
Note Soumu <soumunote@gmail.com>
2020-10-06 08:23:31 +0900

    umakuittayo!

--- EasyOCR10/ScanningDialog.xaml.cs
+++ EasyOCR10/ScanningDialog.xaml.cs
@@ -9,6 +9,7 @@ using Windows.Foundation.Collections;
 using Windows.Graphics.Imaging;
 using Windows.Media.Ocr;
 using Windows.Storage;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -45,22 +46,54 @@ namespace EasyOCR10
                 //TODO
                 //rootPage.NotifyUser();
             }
+            // https://codezine.jp/article/detail/10748?p=3
+            // https://qiita.com/kob58im/items/cdc0dbb93cf7f00e5144
+            // https://rpa.bigtreetc.com/column/microsoftocr/
+            // https://ishigame-machine-technology.net/IMT/archives/1306
+            // https://stackoverflow.com/questions/46544423/how-to-crop-softwarebitmap-in-uwp-c-sharp
+            // https://docs.microsoft.com/ja-jp/windows/uwp/design/controls-and-patterns/dialogs-and-flyouts/dialogs
             // https://github.com/microsoft/Windows-universal-samples/blob/master/Samples/OCR/cs/OcrCapturedImage.xaml.cs
             // https://docs.microsoft.com/ja-jp/windows/uwp/files/quickstart-reading-and-writing-files
             // https://docs.microsoft.com/ja-jp/windows/uwp/audio-video-camera/imaging
             // https://codezine.jp/article/detail/11072?p=2
             // https://www.tnksoft.com/blog/?p=5634
-            var storageFolder = await Windows.Storage.StorageFolder.GetFolderFromPathAsync(@"c:\tmp\ticket");
-            var imageFile = await storageFolder.GetFileAsync("1.jpg");
-            using (var stream = await imageFile.OpenAsync(FileAccessMode.Read))
+            var imageExtentions = new[] { "jpg", "jpeg", "png", "bmp" };
+
+            var folder = await Windows.Storage.StorageFolder.GetFolderFromPathAsync(@"c:\tmp\ticket");
+            var files = await folder.GetFilesAsync();
+            var imageFiles = files.Where(file => imageExtentions.Contains(file.FileType)).ToArray();
+            foreach (var imageFile in files.ToArray())
             {
-                var decoder = await BitmapDecoder.CreateAsync(stream);
-                var bitmap = await decoder.GetSoftwareBitmapAsync();
-                var ocrResult = await ocrEngine.RecognizeAsync(bitmap);
-                var text = ocrResult.Text;
+                using (var stream = await imageFile.OpenAsync(FileAccessMode.Read))
+                {
+                    var decoder = await BitmapDecoder.CreateAsync(stream);
+                    var bitmap = await decoder.GetSoftwareBitmapAsync();
+
+                    var boundsAry = new[]
+                    {
+                        new BitmapBounds
+                        {
+                            X      =        1500, Y      =        75, 
+                            Width  = 1875 - 1500, Height =  170 -  75
+                        },
+                         new BitmapBounds
+                        {
+                            X     =         720, Y      =        690,
+                            Width = 1230 -  720, Height =  800 - 690
+                        },
+                    };
+
+                    foreach (var bounds in boundsAry)
+                    {
+                        var fieldBitmap = await bitmap.Crop(bounds);
+                        var ocrResult = await ocrEngine.RecognizeAsync(fieldBitmap);
+                        var text = ocrResult.Text;
+                        await (new MessageDialog(text)).ShowAsync();
+                    };
+
+                }
 
             }
-
         }
 
 
---
+++ EasyOCR10/SoftwareBitmapExtention.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.Storage.Streams;
+
+namespace EasyOCR10
+{
+    static class SoftwareBitmapExtention
+    {
+        public async static Task<SoftwareBitmap> Crop(this SoftwareBitmap bitmap, BitmapBounds bounds)
+        {
+            using (InMemoryRandomAccessStream stream = new InMemoryRandomAccessStream())
+            {
+                var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.BmpEncoderId, stream);
+                encoder.SetSoftwareBitmap(bitmap);
+                encoder.BitmapTransform.Bounds = bounds;
+
+                await encoder.FlushAsync();
+
+                var decoder = await BitmapDecoder.CreateAsync(stream);
+                return await decoder.GetSoftwareBitmapAsync(bitmap.BitmapPixelFormat, BitmapAlphaMode.Premultiplied);
+            }
+        }
+
+    }
+}
